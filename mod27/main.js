a3fdccb9bdf506caa714a490d7dc30ce1664f8ea
buna212 <bunaberisha27@outlook.com>
2025-09-25 20:44:27 +0200

    mod27

---
+++ mod27/main.js
@@ -0,0 +1,20 @@
+function buna(){
+    alert("This is inside the funtion")
+}
+buna()
+
+function una(number1,number2){
+    return  number1+number2
+}
+console.log(una(10,10))
+
+function toCelsius(numri1){
+    return numri1*5/9
+}
+console.log ("54 farinheit eshte e barabart me"+toCelsius(54)+"celsius")
+
+function drin (){
+    var localVar="I am buna";
+    alert("hello i am BUNA");
+}
+drin();
